0e6b4f799661478c77d0240759fe069e0d5d0579
zakariasi <zsiringoringo@gmail.com>
2020-03-21 23:21:19 +0700

    initial commit

---
+++ src/main/java/com/dicoding/linebotjava/Controller.java
@@ -0,0 +1,83 @@
+package com.dicoding.linebotjava;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.client.LineSignatureValidator;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    @Qualifier("lineMessagingClient")
+    private LineMessagingClient lineMessagingClient;
+
+    @Autowired
+    @Qualifier("lineSignatureValidator")
+    private LineSignatureValidator lineSignatureValidator;
+
+    @RequestMapping(value="/webhook", method= RequestMethod.POST)
+    public ResponseEntity<String> callback(
+            @RequestHeader("X-Line-Signature") String xLineSignature,
+            @RequestBody String eventsPayload)
+    {
+        try {
+            if (!lineSignatureValidator.validateSignature(eventsPayload.getBytes(), xLineSignature)) {
+                throw new RuntimeException("Invalid Signature Validation");
+            }
+
+            // parsing event
+            ObjectMapper objectMapper = ModelObjectMapper.createNewObjectMapper();
+            EventsModel eventsModel = objectMapper.readValue(eventsPayload, EventsModel.class);
+
+            eventsModel.getEvents().forEach((event)->{
+                if (event instanceof MessageEvent) {
+                    MessageEvent messageEvent = (MessageEvent) event;
+                    TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+                    replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+                }
+            });
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    private void reply(ReplyMessage replyMessage) {
+        try {
+            lineMessagingClient.replyMessage(replyMessage).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private void replyText(String replyToken, String messageToUser){
+        TextMessage textMessage = new TextMessage(messageToUser);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, textMessage);
+        reply(replyMessage);
+    }
+
+    private void replySticker(String replyToken, String packageId, String stickerId){
+        StickerMessage stickerMessage = new StickerMessage(packageId, stickerId);
+        ReplyMessage replyMessage = new ReplyMessage(replyToken, stickerMessage);
+        reply(replyMessage);
+    }
+
+
+}
