fd62133ad8f6ca9b33d34593bc3aba8e38fb96ea
zakariasi <zsiringoringo@gmail.com>
2020-03-23 17:09:03 +0700

    implementasi profile

--- src/main/java/com/dicoding/linebotjava/Controller.java
+++ src/main/java/com/dicoding/linebotjava/Controller.java
@@ -11,6 +11,7 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
+import com.linecorp.bot.model.profile.UserProfileResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -125,6 +126,31 @@ public class Controller {
 
         try {
             lineMessagingClient.multicast(multicast).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public ResponseEntity<String> profile(){
+        String userId = "U5033a111bff36e72d69ca748ec1bc9d5";
+        UserProfileResponse profile = getProfile(userId);
+
+        if (profile != null) {
+            String profileName = profile.getDisplayName();
+            TextMessage textMessage = new TextMessage("Hello, " + profileName);
+            PushMessage pushMessage = new PushMessage(userId, textMessage);
+            push(pushMessage);
+
+            return new ResponseEntity<String>("Hello, "+profileName, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+    }
+
+    private UserProfileResponse getProfile(String userId){
+        try {
+            return lineMessagingClient.getProfile(userId).get();
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
