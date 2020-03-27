ed50e2b373ebfe6b7fbffdea976d29294cfeb7ad
zakariasi <zsiringoringo@gmail.com>
2020-03-27 21:37:56 +0700

    implementasi group/room api

--- src/main/java/com/dicoding/linebotjava/Controller.java
+++ src/main/java/com/dicoding/linebotjava/Controller.java
@@ -9,6 +9,8 @@ import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.*;
+import com.linecorp.bot.model.event.source.GroupSource;
+import com.linecorp.bot.model.event.source.RoomSource;
 import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
@@ -54,25 +56,35 @@ public class Controller {
             ObjectMapper objectMapper = ModelObjectMapper.createNewObjectMapper();
             EventsModel eventsModel = objectMapper.readValue(eventsPayload, EventsModel.class);
 
+//            eventsModel.getEvents().forEach((event)->{
+//                if (event instanceof MessageEvent) {
+//                    if  ((  (MessageEvent) event).getMessage() instanceof AudioMessageContent
+//                            || ((MessageEvent) event).getMessage() instanceof ImageMessageContent
+//                            || ((MessageEvent) event).getMessage() instanceof VideoMessageContent
+//                            || ((MessageEvent) event).getMessage() instanceof FileMessageContent
+//                    ) {
+//                        String baseURL     = "https://linebot-zakkariassi.herokuapp.com";
+//                        String contentURL  = baseURL+"/content/"+ ((MessageEvent) event).getMessage().getId();
+//                        String contentType = ((MessageEvent) event).getMessage().getClass().getSimpleName();
+//                        String textMsg     = contentType.substring(0, contentType.length() -14)
+//                                + " yang kamu kirim bisa diakses dari link:\n "
+//                                + contentURL;
+//
+//                        replyText(((MessageEvent) event).getReplyToken(), textMsg);
+//                    } else {
+//                        MessageEvent messageEvent = (MessageEvent) event;
+//                        TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+//                        replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+//                    }
+//                }
+//            });
+
             eventsModel.getEvents().forEach((event)->{
                 if (event instanceof MessageEvent) {
-                    if  ((  (MessageEvent) event).getMessage() instanceof AudioMessageContent
-                            || ((MessageEvent) event).getMessage() instanceof ImageMessageContent
-                            || ((MessageEvent) event).getMessage() instanceof VideoMessageContent
-                            || ((MessageEvent) event).getMessage() instanceof FileMessageContent
-                    ) {
-                        String baseURL     = "https://linebot-zakkariassi.herokuapp.com";
-                        String contentURL  = baseURL+"/content/"+ ((MessageEvent) event).getMessage().getId();
-                        String contentType = ((MessageEvent) event).getMessage().getClass().getSimpleName();
-                        String textMsg     = contentType.substring(0, contentType.length() -14)
-                                + " yang kamu kirim bisa diakses dari link:\n "
-                                + contentURL;
-
-                        replyText(((MessageEvent) event).getReplyToken(), textMsg);
+                    if (event.getSource() instanceof GroupSource || event.getSource() instanceof RoomSource) {
+                        handleGroupRoomChats((MessageEvent) event);
                     } else {
-                        MessageEvent messageEvent = (MessageEvent) event;
-                        TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
-                        replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+                        handleOneOnOneChats((MessageEvent) event);
                     }
                 }
             });
@@ -83,6 +95,51 @@ public class Controller {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
+    }
+
+    private void handleOneOnOneChats(MessageEvent event) {
+        if  (event.getMessage() instanceof AudioMessageContent
+                || event.getMessage() instanceof ImageMessageContent
+                || event.getMessage() instanceof VideoMessageContent
+                || event.getMessage() instanceof FileMessageContent
+        ) {
+            handleContentMessage(event);
+        } else if(event.getMessage() instanceof TextMessageContent) {
+            handleTextMessage(event);
+        } else {
+            replyText(event.getReplyToken(), "Unknown Message");
+        }
+    }
+
+    private void handleGroupRoomChats(MessageEvent event) {
+        if(!event.getSource().getUserId().isEmpty()) {
+            String userId = event.getSource().getUserId();
+            UserProfileResponse profile = getProfile(userId);
+            replyText(event.getReplyToken(), "Hello, " + profile.getDisplayName());
+        } else {
+            replyText(event.getReplyToken(), "Hello, what is your name?");
+        }
+    }
+
+    private void handleContentMessage(MessageEvent event) {
+        String baseURL     = "https://linebot-zakkariassi.herokuapp.com";
+        String contentURL  = baseURL+"/content/"+ event.getMessage().getId();
+        String contentType = event.getMessage().getClass().getSimpleName();
+        String textMsg     = contentType.substring(0, contentType.length() -14)
+                + " yang kamu kirim bisa diakses dari link:\n "
+                + contentURL;
+
+        replyText(event.getReplyToken(), textMsg);
+    }
+
+    private void handleTextMessage(MessageEvent event) {
+        TextMessageContent textMessageContent = (TextMessageContent) event.getMessage();
+
+        if (textMessageContent.getText().toLowerCase().contains("flex")) {
+            replyFlexMessage(event.getReplyToken());
+        } else {
+            replyText(event.getReplyToken(), textMessageContent.getText());
+        }
     }
 
     @RequestMapping(value = "/content/{id}", method = RequestMethod.GET)
