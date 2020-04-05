6247109ed64f2f0d09eab11ed3bb52b8e098b651
zakariasi <zsiringoringo@gmail.com>
2020-04-05 19:20:20 +0700

    covid

---
+++ src/main/java/com/dicoding/linebotjava/CovidEvents.java
@@ -0,0 +1,41 @@
+package com.dicoding.linebotjava;
+
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "success",
+        "data"
+})
+public class CovidEvents {
+
+    @JsonProperty("success")
+    private boolean success;
+    @JsonProperty("data")
+    private List<Datum> data = null;
+
+    @JsonProperty("success")
+    public boolean isSuccess() {
+        return success;
+    }
+
+    @JsonProperty("success")
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    @JsonProperty("data")
+    public List<Datum> getData() {
+        return data;
+    }
+
+    @JsonProperty("data")
+    public void setData(List<Datum> data) {
+        this.data = data;
+    }
+
+}
