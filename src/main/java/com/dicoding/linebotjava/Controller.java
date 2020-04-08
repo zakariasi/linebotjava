19df632a1cbbc747daca36575a0b37f62638644f
zakariasi <zsiringoringo@gmail.com>
2020-04-08 11:17:07 +0700

    a

--- src/main/java/com/dicoding/linebotjava/Controller.java
+++ src/main/java/com/dicoding/linebotjava/Controller.java
@@ -157,13 +157,24 @@ public class Controller {
 
     private void handleTextMessage(MessageEvent event) {
         TextMessageContent textMessageContent = (TextMessageContent) event.getMessage();
+        getCovidEventsData();
+
+
+
+        Datum eventData = covidEvents.getData().get(0);;
+
+
+
+
+        int fid = eventData.getFid();
+        String a = String.valueOf(fid);
 
         if (textMessageContent.getText().toLowerCase().contains("flex")) {
             replyFlexMessage(event.getReplyToken());
         } else if(textMessageContent.getText().toLowerCase().contains("covid")) {
-            showEventSummary(event.getReplyToken());
-        } else {
             replyText(event.getReplyToken(), textMessageContent.getText());
+        } else {
+            replyText(event.getReplyToken(), a);
         }
     }
 
