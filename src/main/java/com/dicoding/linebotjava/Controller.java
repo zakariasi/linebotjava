7996bc58a9d7ec0dd5aa27ac42e8cdc7ea540d5b
zakariasi <zsiringoringo@gmail.com>
2020-04-11 10:17:26 +0700

    testt

--- src/main/java/com/dicoding/linebotjava/Controller.java
+++ src/main/java/com/dicoding/linebotjava/Controller.java
@@ -175,7 +175,7 @@ public class Controller {
         } else if(textMessageContent.getText().toLowerCase().contains("a3")) {
             replyText(event.getReplyToken(), daftarKecamatanMedan);
         } else if(textMessageContent.getText().toLowerCase().contains("kc")) {
-            replyText(event.getReplyToken(), dataKecamatan(textMessageContent.getText())) );
+            replyText(event.getReplyToken(), dataKecamatan(textMessageContent.getText()));
         }
     }
 
